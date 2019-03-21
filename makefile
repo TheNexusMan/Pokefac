fb965558cdb3cec46234a2a6bc36ad6157ea9cb2
DEBRABANT ARNAUD p1707147 <arnaud.debrabant@etu.univ-lyon1.fr>
2019-03-21 23:13:19 +0100

    Fix problemes de merge

--- makefile
+++ makefile
@@ -1,12 +1,7 @@
 all: bin/pokefac
 
-<<<<<<< HEAD
-bin/pokefac: obj/main.o obj/pokemon.o obj/character.o obj/player.o obj/NPC.o obj/terrain.o obj/world.o
-	g++ -g -Wall obj/main.o obj/pokemon.o obj/character.o obj/player.o obj/NPC.o obj/terrain.o obj/world.o -o bin/pokefac
-=======
 bin/pokefac: obj/main.o obj/pokemon.o obj/character.o obj/player.o obj/NPC.o obj/terrain.o obj/world.o obj/loopTxT.o obj/winTxt.o
 	g++ -g -Wall obj/main.o obj/pokemon.o obj/character.o obj/player.o obj/NPC.o obj/terrain.o obj/world.o obj/loopTxT.o obj/winTxt.o -o bin/pokefac
->>>>>>> arnaud
 
 obj/main.o: src/main.cpp src/terrain.h src/world.h
 	g++ -g -Wall -c src/main.cpp -o obj/main.o
@@ -29,8 +24,6 @@ obj/NPC.o: src/NPC.cpp src/NPC.h
 obj/character.o: src/character.cpp src/character.h
 	g++ -g -Wall -c src/character.cpp -o obj/character.o
 
-<<<<<<< HEAD
-=======
 obj/loopTxT.o: src/loopTxT.cpp src/loopTxT.h
 	g++ -g -Wall -c src/loopTxT.cpp -o obj/loopTxT.o
 
@@ -38,7 +31,6 @@ obj/winTxt.o: src/winTxt.cpp src/winTxt.h
 	g++ -g -Wall -c src/winTxt.cpp -o obj/winTxt.o
 
 
->>>>>>> arnaud
 clean:
 	rm -f obj/*.o
 	rm -f bin/*
--- src/main.cpp
+++ src/main.cpp
@@ -7,29 +7,29 @@ int main(void)
 {
 	world mainWorld;
 
-	// srand(time(NULL));
-	// termClear();
+	srand(time(NULL));
+	termClear();
 
-	// txtLoop(mainWorld);
-	// termClear();
+	txtLoop(mainWorld);
+	termClear();
 
-	//TEST initPokeTab
-	// cout << mainWorld.poke[0].id << endl;
-	// cout << mainWorld.poke[0].name << endl;
-	// cout << mainWorld.poke[0].type << endl;
-	// cout << mainWorld.poke[0].attackChoice[0].name << endl << endl;
+	// TEST initPokeTab
+	// cout << mainWorld.pokeTab[0].id << endl;
+	// cout << mainWorld.pokeTab[0].name << endl;
+	// cout << mainWorld.pokeTab[0].type << endl;
+	// cout << mainWorld.pokeTab[0].attackChoice[0].name << endl << endl;
 
-	// cout << mainWorld.poke[1].id << endl;
-	// cout << mainWorld.poke[1].name << endl;
-	// cout << mainWorld.poke[1].type << endl;
-	// cout << mainWorld.poke[1].attackChoice[0].name << endl << endl;
+	// cout << mainWorld.pokeTab[1].id << endl;
+	// cout << mainWorld.pokeTab[1].name << endl;
+	// cout << mainWorld.pokeTab[1].type << endl;
+	// cout << mainWorld.pokeTab[1].attackChoice[0].name << endl << endl;
 
-	// cout << mainWorld.poke[2].id << endl;
-	// cout << mainWorld.poke[2].name << endl;
-	// cout << mainWorld.poke[2].type << endl;
-	// cout << mainWorld.poke[2].attackChoice[0].name << endl << endl;
+	// cout << mainWorld.pokeTab[2].id << endl;
+	// cout << mainWorld.pokeTab[2].name << endl;
+	// cout << mainWorld.pokeTab[2].type << endl;
+	// cout << mainWorld.pokeTab[2].attackChoice[0].name << endl << endl;
 
-	//TEST initNPCTab
+	// TEST initNPCTab
 	// for(unsigned int i = 0; i < NB_NPC; i++)
 	// {
 	// 	cout << mainWorld.NPCTab[i].id << endl;
