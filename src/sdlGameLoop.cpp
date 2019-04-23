04b47499721c6a891e13eae6128623a74c738593
Kelly <kelly.de-oliveira@etu.univ-lyon1.fr>
2019-04-23 12:41:59 +0200

    test 2 musique

--- src/sdlGameLoop.cpp
+++ src/sdlGameLoop.cpp
@@ -451,16 +451,13 @@ void SdlGame::sdlLoop(world &world)
 
     Uint32 deltaTime = SDL_GetTicks(), elapsedTime;
 
-    if( Mix_PlayingMusic() == 0)
-    {
         if (Mix_PlayMusic( sound, -1 ) == -1)
         {
             return 1;
         }
-    } 
                    
 
-    while(!quit)
+    while(!quift)
     {
         SDL_FlushEvent(events.type);
         while (SDL_PollEvent(&events))
